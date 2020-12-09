a87f2ebb579b2335c0600940e02de1ef36793fa1
Egor Sirosh <egor.thanatos@gmail.com>
2020-12-09 20:43:05 +0300

    modified TommyHilfigerTest.java

--- src/test/java/TommyHilfigerTest.java
+++ src/test/java/TommyHilfigerTest.java
@@ -20,65 +20,62 @@ public  class TommyHilfigerTest {
     private WebDriver driver;
 
     @BeforeMethod(alwaysRun = true)
-    public void BrowserSetup(){
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        driver = new ChromeDriver(chromeOptions);
+    public void browserSetup(){
+        driver = new ChromeDriver();
     }
 
-
-
     @Test
-    public void SearchItemTest() throws InterruptedException {
+    public void searchItemTest() throws InterruptedException {
 
         driver.get("https://ru.tommy.com/%D0%BC%D1%83%D0%B6%D1%87%D0%B8%D0%BD%D1%8B");
-        WebElement searchCookiesButton = WaitForElementsLocatedBy(driver,
+        WebElement cookiesPoliticsButton = waitForElementsLocatedBy(driver,
                 By.xpath("//button[@class='cookie-notice__agree-button cookie-notice__agree-button--ru button']"));
-        searchCookiesButton.click();
-        WebElement searchInput = WaitForElementsLocatedBy(driver, By.id("headerSearchInput"));
-        searchInput.click();
-        searchInput.sendKeys("MW0MW12983_BD2");
-        searchInput.sendKeys(Keys.ENTER);
-        List<WebElement> seacrhResults = driver.findElements(By.xpath("//li[@class='product-list__item three']/article"));
-        Assert.assertTrue(seacrhResults.get(0).getAttribute("data-primarycolour").equals("MW0MW12983_BD2"), "Item not found");
+        cookiesPoliticsButton.click();
+        WebElement headerInput = waitForElementsLocatedBy(driver, By.id("headerSearchInput"));
+        headerInput.click();
+        headerInput.sendKeys("MW0MW12983_BD2");
+        headerInput.sendKeys(Keys.ENTER);
+        List<WebElement> seacrhingResults = driver
+                .findElements(By.xpath("//li[@class='product-list__item three']/article"));
+        Assert.assertTrue(seacrhingResults.get(0).getAttribute("data-primarycolour")
+                .equals("MW0MW12983_BD2"), "Item not found");
 
     }
 
     @Test
-    public void AddItemInWishlistTest() throws InterruptedException {
-
+    public void addItemInWishlistTest() throws InterruptedException {
         driver.get("https://ru.tommy.com/%D0%BC%D1%83%D0%B6%D1%87%D0%B8%D0%BD%D1%8B");
-        WebElement searchCookiesButton = WaitForElementsLocatedBy(driver,
+        WebElement сookiesPoliticsButton = waitForElementsLocatedBy(driver,
                 By.xpath("//button[@class='cookie-notice__agree-button cookie-notice__agree-button--ru button']"));
-        searchCookiesButton.click();
-        WebElement goToShopButton = WaitForElementsLocatedBy(driver,
-                By.xpath("//span[@class='th-cta-wrapper th-cta-wrapper-vertical ']/a[text()=' ПЕРЕЙТИ В МАГАЗИН ']"));
-        goToShopButton.click();
+        сookiesPoliticsButton.click();
+        WebElement mensClothingButton = waitForElementsLocatedBy(driver,
+                By.xpath("//a[@href='https://ru.tommy.com/мужчины']/span"));
+        mensClothingButton.click();
+        WebElement festiveStyleButton = waitForElementsLocatedBy(driver,
+                By.xpath("//a[@data-track-container-of-cta-id='3243130']"));
+        festiveStyleButton.click();
         List<WebElement> firstItemInList = driver.findElements(By.xpath("//ul[@class='product-list__items']/li"));
         firstItemInList.get(0).click();
-        WebElement addingWishlistToggle = WaitForElementsLocatedBy(driver,
+        WebElement addingWishlistToggle = waitForElementsLocatedBy(driver,
                 By.xpath("//span[@class='wishlist__toggle--button']"));
         addingWishlistToggle.click();
-        WebElement wishlistButton = WaitForElementsLocatedBy(driver,
+        WebElement wishlistButton = waitForElementsLocatedBy(driver,
                 By.xpath("//button[@class='wishlist__button']"));
         wishlistButton.click();
-
-        WebDriverWait waitForElement = new WebDriverWait(driver, 10);
-        waitForElement.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='wishlist-item']")));
-
+        WebDriverWait waitForElementToBeVisible = new WebDriverWait(driver, 10);
+        waitForElementToBeVisible
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='wishlist-item']")));
         List<WebElement> itemsInWishlist = driver.findElements(By.xpath("//div[@class='wishlist-item']"));
         Assert.assertTrue(itemsInWishlist.size()>0,"Item not found");
-
     }
 
-    private static WebElement WaitForElementsLocatedBy(WebDriver driver, By by){
+    private static WebElement waitForElementsLocatedBy(WebDriver driver, By by){
         return new WebDriverWait(driver, 10)
                 .until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
-
     @AfterMethod(alwaysRun = true)
-    public void BrowserTearDown() {
+    public void browserTearDown() {
         driver.quit();
     }
 
